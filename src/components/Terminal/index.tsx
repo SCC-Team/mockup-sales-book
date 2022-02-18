9892a9bcef56ca5156a03753aa6b462e0a8a5778
Camilo <cgonzalezm1234@gmail.com>
2022-02-18 10:39:56 -0300

    fix: observations

--- src/components/Terminal/index.tsx
+++ src/components/Terminal/index.tsx
@@ -4,6 +4,10 @@ import moment from 'moment-timezone';
 import {TerminalCalendar} from '@scc/react-big-calendar';
 
 import terminalCalendarMock from './../../mock/terminalCalendar.json';
+import terminalCalendarDetailMock from './../../mock/terminalCalendarDetail.json';
+import terminalCalendarDetailPendingMock from './../../mock/terminalCalendarDetailPending.json';
+import terminalCalendarDetailPendingIncomeMock from './../../mock/terminalCalendarDetailPendingIncome.json';
+import terminalCalendarDetailSquareMock from './../../mock/terminalCalendarDetailSquare.json';
 import allLocalMock from './../../mock/allLocal.json';
 import allTerminalMock from './../../mock/allTerminal.json';
 import terminalPendingMock from './../../mock/terminalPending.json';
@@ -16,66 +20,111 @@ export const Terminal = () => {
   const [date, setDate] = useState(new Date());
   const [events, setEvents] = useState<any>([]);
   const [allLocal, setAllLocal] = useState<any>([]);
-  const [allTerminal, setTerminal] = useState<any>([]);
+  const [allTerminal, setAllTerminal] = useState<any>([]);
+  const [terminal, setTerminal] = useState({
+    terminal: '',
+    status: '',
+    show: false,
+  });
 
   const onSelectEvent = useCallback((item) => {
-    getStatusTerminal(item.type)
+    getStatusTerminal(item.type, item.clickeable);
   }, []);
 
-  const terminalDetailClick = (type: any) => {
-    console.log(type)
-    getStatusTerminal(type)
-  }
-
-  const getStatusTerminal = (type: any) => {
+  const terminalDetailClick = (terminal: any, status: any) => {
+    setAllTerminal([]);
     setAllLocal([]);
-    if(type === 'pendingIncome'){
-      const result: any = terminalPendingIncomeMock;
-      setTerminal(result.data.results);
+    setTerminal({
+      terminal: terminal,
+      status: status,
+      show: true,
+    });
+
+    if (status === 'pending') {
+      setDataCalendar(terminalCalendarDetailPendingMock, false);
     }
-    if(type === 'pending'){
-      const result: any = terminalPendingMock;
-      setTerminal(result.data.results);
+    if (status === 'pendingIncome') {
+      setDataCalendar(terminalCalendarDetailPendingIncomeMock, false);
     }
-    if(type === 'square'){
-      const result: any = terminalSquareMock;
-      setTerminal(result.data.results);
+    if (status === 'square') {
+      setDataCalendar(terminalCalendarDetailSquareMock, false);
     }
-  }
+  };
+
+  const getStatusTerminal = (type: any, clickeable: any) => {
+    if (clickeable) {
+      setAllLocal([]);
+      setTerminal({
+        terminal: '',
+        status: '',
+        show: false,
+      });
+      if (type === 'pendingIncome') {
+        const result: any = terminalPendingIncomeMock;
+        setAllTerminal(result.data.results);
+      }
+      if (type === 'pending') {
+        const result: any = terminalPendingMock;
+        setAllTerminal(result.data.results);
+      }
+      if (type === 'square') {
+        const result: any = terminalSquareMock;
+        setAllTerminal(result.data.results);
+      }
+      if (type === 'all') {
+        const result: any = allTerminalMock;
+        setAllTerminal(result.data.results);
+      }
+    }
+  };
 
   const handleDate = (e: any) => {
     const value = e;
     setDate(value);
   };
 
-  const handleInputChangeLocal = (e: any) =>  {
+  const handleInputChangeLocal = (e: any) => {
     const value = e.target.value;
-    
-    if(value === 'all'){
+
+    handleLocal(value);
+  };
+
+  const handleLocal = (option: any) => {
+    if (option === 'all') {
       const result: any = allLocalMock;
       setAllLocal(result.data.results);
-      setTerminal([]);
+      setDataCalendar(terminalCalendarMock, true);
+      setAllTerminal([]);
     }
-    if(value === 'arica'){
+    if (option === 'arica') {
       const result: any = allTerminalMock;
-      setTerminal(result.data.results);
+      setAllTerminal(result.data.results);
+      setDataCalendar(terminalCalendarDetailMock, true);
       setAllLocal([]);
     }
-    // debugger; // eslint-disable-line no-debugger
-  } 
+  };
 
-  const allLocalClick = (local: any) => {
+  const allLocalClick = () => {
     const result: any = allTerminalMock;
-    setTerminal(result.data.results);
+    setDataCalendar(terminalCalendarDetailMock, true);
+    setAllTerminal(result.data.results);
     setAllLocal([]);
-  }
+    setTerminal({
+      terminal: '',
+      status: '',
+      show: false,
+    });
+  };
 
-  useEffect(() => {
-
+  const setDataCalendar = (calendar: any, count: any) => {
     //Calendar
-    if (terminalCalendarMock.data.pendingIncome.length > 0) {
-      let eventsResult = [];
-      const pendingIncome = terminalCalendarMock.data.pendingIncome.map((item: any) => {
+    let eventsResult = [];
+    let pendingIncome = [];
+    let pending = [];
+    let square = [];
+    let all = [];
+    if (calendar.data.pendingIncome.length > 0) {
+      pendingIncome = calendar.data.pendingIncome.map((item: any) => {
         return {
           title: item.title,
           start: moment(item.date).utc().format('YYYY-MM-DD'),
@@ -86,10 +135,13 @@ export const Terminal = () => {
           color: '#897711',
           success: true,
           type: 'pendingIncome',
+          seeDay: false,
+          seeCount: count,
         };
       });
-
-      const pending = terminalCalendarMock.data.pending.map((item: any) => {
+    }
+    if (calendar.data.pending.length > 0) {
+      pending = calendar.data.pending.map((item: any) => {
         return {
           title: item.title,
           start: moment(item.date).utc().format('YYYY-MM-DD'),
@@ -100,10 +152,14 @@ export const Terminal = () => {
           color: '#ee0c64',
           success: true,
           type: 'pending',
+          seeDay: false,
+          seeCount: count,
         };
       });
+    }
 
-      const square = terminalCalendarMock.data.square.map((item: any) => {
+    if (calendar.data.square.length > 0) {
+      square = calendar.data.square.map((item: any) => {
         return {
           title: item.title,
           start: moment(item.date).utc().format('YYYY-MM-DD'),
@@ -114,20 +170,39 @@ export const Terminal = () => {
           color: '#238f51',
           success: true,
           type: 'square',
+          seeDay: false,
+          seeCount: count,
         };
       });
-
-      eventsResult = pendingIncome.concat(pending).concat(square);
-      setEvents(eventsResult);
-
-
-      //Sidebar
-      const result: any = allLocalMock;
-      setAllLocal(result.data.results);
-
-    } else {
-      setEvents([]);
     }
+
+    if (calendar.data.all.length > 0) {
+      all = calendar.data.all.map((item: any) => {
+        return {
+          title: item.title,
+          start: moment(item.date).utc().format('YYYY-MM-DD'),
+          end: moment(item.date).utc().format('YYYY-MM-DD'),
+          description: '',
+          count: item.count,
+          bgcolor: '#0058a3',
+          color: '#fff',
+          success: true,
+          type: 'all',
+          seeDay: true,
+          seeCount: false,
+        };
+      });
+    }
+    eventsResult = pendingIncome.concat(pending).concat(square).concat(all);
+    setEvents(eventsResult);
+  };
+
+  useEffect(() => {
+    //Calendar
+    setDataCalendar(terminalCalendarMock, true);
+    //Sidebar
+    const result: any = allLocalMock;
+    setAllLocal(result.data.results);
   }, []);
 
   return (
@@ -152,7 +227,7 @@ export const Terminal = () => {
             <div className="flex-item flex-item__3 mb-0">
               <span className="label-input">Locales</span>
               <select name="local" onChange={handleInputChangeLocal}>
-                <option value="all" >Todos</option>
+                <option value="all">Todos</option>
                 <option value="arica">Expo Arica</option>
               </select>
             </div>
@@ -164,28 +239,89 @@ export const Terminal = () => {
           <section className="flex-container">
             <div className="flex-container flex--center flex-item--top sidebar">
               <div className="flex-item mr-0 mb-0">
-                <div className="flex-container btn-group mt-4">
+                <div className="btn-group pt-1">
                   {/* Todos los locales */}
-                {allLocal.length > 0
-                && allLocal.map((data: any, i: any) => (
-                  <button className="flex-item btn mb-0" key={ i + 1 } onClick={ () => allLocalClick( data.local)}>
-                    <span className="flex-item left">{ data.local }</span>
-                    <div className="right">
-                    <span className="flex-item count pendingIncome">{ data.pendingIncome }</span>
-                    <span className="flex-item count pending">{ data.pending }</span>
-                    <span className="flex-item count square">{ data.square }</span>
-                    </div>
-                  </button>
-                )) }
 
                   {/* Detalle por local */}
-                {allTerminal.length > 0
-                && allTerminal.map((data: any, i: any) => (
-                  <button className="flex-item_19 btn mb-0 w-100 terminal" key={ i + 1 }  onClick={ () => terminalDetailClick( data.status)}>
-                    <span className="flex-item left">{ data.terminal }</span>
-                    <span className={ `right flex-item count ${ data.status }`}></span>
-                  </button>
-                )) }
+                  {allLocal.length > 0 && (
+                    <span className="label-input flex-container normal-case mb-2">
+                      Listado de Locales
+                    </span>
+                  )}
+
+                  {allLocal.length > 0 &&
+                    allLocal.map((data: any, i: any) => (
+                      <button
+                        className="flex-container flex-item btn mb-0 w-100"
+                        key={i + 1}
+                        onClick={() => allLocalClick()}>
+                        <span className="flex-item text-left flex-left">
+                          {data.local}
+                        </span>
+                        <div>
+                          <span className="flex-item count pendingIncome">
+                            {data.pendingIncome}
+                          </span>
+                          <span className="flex-item count pending">
+                            {data.pending}
+                          </span>
+                          <span className="flex-item count square">
+                            {data.square}
+                          </span>
+                        </div>
+                      </button>
+                    ))}
+
+                  {/* Detalle por local */}
+                  {allTerminal.length > 0 && (
+                    <>
+                      <span className="label-input normal-case">
+                        Listado de Terminales
+                      </span>
+                      <button
+                        className="btn btn-secondary float-right mr-1"
+                        onClick={() => handleLocal('all')}>
+                        <span className="icofont-exit"></span>Volver
+                      </button>
+                    </>
+                  )}
+
+                  {allTerminal.length > 0 &&
+                    allTerminal.map((data: any, i: any) => (
+                      <button
+                        className="flex-item_19 btn mb-0 w-11/12 terminal"
+                        key={i + 1}
+                        onClick={() =>
+                          terminalDetailClick(data.terminal, data.status)
+                        }>
+                        <span className="flex-item left">{data.terminal}</span>
+                        <span
+                          className={`right flex-item count text-sm ${data.status}`}></span>
+                      </button>
+                    ))}
+
+                  {/* Detalle por terminal */}
+                  {terminal.show && (
+                    <>
+                      <span className="label-input normal-case">
+                        Detalle del Terminal
+                      </span>
+                      <button
+                        className="btn btn-secondary float-right mr-1"
+                        onClick={() => allLocalClick()}>
+                        <span className="icofont-exit"></span>Volver
+                      </button>
+                    </>
+                  )}
+                  {terminal.show && (
+                    <button className="flex-item_19 btn mb-0 w-11/12 terminal terminal-detail">
+                      <span className="flex-item left">
+                        {terminal.terminal}
+                      </span>
+                      <span
+                        className={`right flex-item count text-sm ${terminal.status}`}></span>
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
